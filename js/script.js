5df2a980248ea0a772bb30d14cfde79a189555fa
Neimat Soliman <neimat.soliman.ismail@gmail.com>
2022-02-17 19:07:02 +0200

    header section ends and scroll window

--- js/script.js
+++ js/script.js
@@ -1,4 +1,40 @@
 
+// start header section
+let navbar = document.querySelector('.header .navbar');
+let menu_btn = document.querySelector(".header .menu-btn");
+
+menu_btn.onclick = () =>{
+
+    document.querySelector(".header .menu-btn i").classList.toggle("fa-bars");
+    document.querySelector(".header .menu-btn i").classList.toggle("fa-times");
+    menu_btn.classList.toggle('active');
+    navbar.classList.toggle('active');
+}
+// end header section
+
+// scroll window
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+
+    if(window.scrollY > 0){
+        document.querySelector('.header').classList.add('active');
+    }else{
+        document.querySelector('.header').classList.remove('active');
+    }
+
+};
+
+window.onload = () =>{
+    if(window.scrollY > 0){
+        document.querySelector('.header').classList.add('active');
+    }else{
+        document.querySelector('.header').classList.remove('active');
+    }
+
+};
+// end scroll window 
+
 document.querySelectorAll(".platform-overview .image .image-contrlos .image-btn ").forEach(btn => {
     btn.onclick =() =>{
         let src =  btn.getAttribute("data-src");
